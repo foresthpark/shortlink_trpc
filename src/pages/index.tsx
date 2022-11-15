6ec388b27aa71516ed7677c154f876e168511baf
Forest Park <foresthpark@gmail.com>
2022-11-14 23:50:23 -0700

    mobile css fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,13 +68,15 @@ const Home: NextPage = () => {
         } flex w-4/5 flex-row items-center justify-start rounded-xl`}
       >
         <div className="flex w-full flex-row items-center justify-start gap-3 p-5">
-          <div className="h-5 w-5 rounded-full bg-red-400"></div>
-          <div className="h-5 w-5 rounded-full bg-yellow-400"></div>
-          <div className="h-5 w-5 rounded-full bg-green-400"></div>
-          <ArrowLeftIcon className="h-8 w-8 text-gray-400" />
-          <ArrowRightIcon className="h-8 w-8 text-gray-400" />
-          <ArrowPathIcon className="h-8 w-8 text-gray-400" />
-          <HomeIcon className="h-8 w-8 text-gray-400" />
+          <div className="hidden flex-row items-center justify-start gap-3 md:flex">
+            <div className="h-5 w-5 rounded-full bg-red-400"></div>
+            <div className="h-5 w-5 rounded-full bg-yellow-400"></div>
+            <div className="h-5 w-5 rounded-full bg-green-400"></div>
+            <ArrowLeftIcon className="h-8 w-8 text-gray-400" />
+            <ArrowRightIcon className="h-8 w-8 text-gray-400" />
+            <ArrowPathIcon className="h-8 w-8 text-gray-400" />
+            <HomeIcon className="h-8 w-8 text-gray-400" />
+          </div>
           <div className="w-100 relative flex flex-grow items-stretch focus-within:z-10">
             <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
               <LockClosedIcon
